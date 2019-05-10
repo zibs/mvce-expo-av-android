8798df18811ff0f9b77dce0bab85866e431fd189
zibs <elizibin@gmail.com>
2019-05-10 16:49:28 -0700

    add expo-av + video component for crashing example

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
-
+import { Video as ExpoVideo } from 'expo-av';
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
@@ -11,8 +11,16 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <ExpoVideo
+          source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+          rate={1.0}
+          volume={1.0}
+          isMuted={false}
+          useNativeControls={true}
+          shouldPlay={true}
+          resizeMode="cover"
+          style={{ width: 300, height: 300 }}
+        />
       </View>
     );
   }
